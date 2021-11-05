266b9ae44cef653dd20503e37277eb1069bbff06
kamrajng <olaitankam2@gmail.com>
2021-11-06 00:59:36 +0100

    updating funtionality

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -6,6 +6,7 @@ const Balance = () => {
   const { transactions } = useContext(GlobalContext);
 
   const amounts = transactions.map((transaction) => transaction.amount);
+  console.log(amounts);
   const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
   return (
     <div>
